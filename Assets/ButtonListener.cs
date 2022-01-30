1c2a80e515b075bd752a47f79676154cc78d571d
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-01-29 22:05:06 -0500

    Added project

---
+++ Assets/ButtonListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonListener : MonoBehaviour
+{
+    public GameObject cube;
+
+    public void Counter()
+    {
+        cube.transform.Rotate(0, 0, 1);
+    }
+    public void Clock()
+    {
+        cube.transform.Rotate(0, 0, -1);
+    }
+}
---
+++ Assets/Degrees.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Degrees : MonoBehaviour
+{
+    public GameObject cube;
+    Text txt;
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = cube.transform.eulerAngles.z + " Degrees";
+    }
+}
